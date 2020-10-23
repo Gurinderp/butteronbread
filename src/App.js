38e9bb91f989abeba363e1ba5563ef0d79aabe27
rednirug <sgurinderpreet@gmail.com>
2020-10-23 15:53:49 -0700

    src changes and reset.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Nav from "./pages/Nav"
+import Home from "./pages/Home"
+import Footer from "./pages/Footer"
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Nav />
+          <Route exact path="/">
+            <Home />
+          </Route>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Nav/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css"
+import { Link } from "react-router-dom";
+
+function Nav() {
+    return(
+        <div>
+            <Link to="/">Home</Link>
+        </div>
+    )
+}
+
+export default Nav;
