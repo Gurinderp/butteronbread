df63c0d426e53ac2aca00217b2fed2608e0a9e41
rednirug <sgurinderpreet@gmail.com>
2020-11-04 03:43:58 -0800

    homepage layout

--- src/pages/Nav/index.js
+++ src/pages/Nav/index.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 function Nav() {
     return(
-        <div>
+        <div className="nav-container">
             <Link to="/">Home</Link>
             <Link to="/products">Products</Link>
             <Link to="/login">Login</Link>
