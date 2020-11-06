277f74258ed8aa0ba0b90b54fecdffeb6caa43a7
rednirug <sgurinderpreet@gmail.com>
2020-11-06 13:00:17 -0800

    product list page

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,24 +1,101 @@
 import React from "react";
 import "./index.css";
 import { Link } from "react-router-dom";
+import placeholder from "../../assets/placeholderimage.png";
 
 function Products() {
 	return (
 		<div>
-			<div>
-				Product 1 <Link to="/products/butteronbread">Bonbread</Link>
+			<div className="card-row">
+				<div className="product-card">
+					<div className="card-title">
+						<Link to="/products/butter">Butter</Link>
+					</div>
+					<img
+						src={placeholder}
+						alt="placeholder"
+						className="card-image"
+					></img>
+					<div className="card-description">
+						<p className="cd-title">Description</p>
+						<p className="cd-description">
+							This will be the product description
+						</p>
+						<p className="cd-price">Price: $100.00</p>
+					</div>
+				</div>
+				<div className="product-card right">
+					<div className="card-title">
+						<Link to="/products/bread">Bread</Link>
+					</div>
+					<img
+						src={placeholder}
+						alt="placeholder"
+						className="card-image"
+					></img>
+					<div className="card-description">
+						<p className="cd-title">Description</p>
+						<p className="cd-description">
+							This will be the product description
+						</p>
+						<p className="cd-price">Price: $100.00</p>
+					</div>
+				</div>
 			</div>
-			<div>
-				Product 2 <Link to="/products/butterontoast">Bontoast</Link>
+			<div className="card-row">
+				<div className="product-card">
+					<div className="card-title">
+						<Link to="/products/butteronbread">Bonbread</Link>
+					</div>
+					<img
+						src={placeholder}
+						alt="placeholder"
+						className="card-image"
+					></img>
+					<div className="card-description">
+						<p className="cd-title">Description</p>
+						<p className="cd-description">
+							This will be the product description
+						</p>
+						<p className="cd-price">Price: $100.00</p>
+					</div>
+				</div>
+				<div className="product-card right">
+					<div className="card-title">
+						<Link to="/products/butterontoast">Bontoast</Link>
+					</div>
+					<img
+						src={placeholder}
+						alt="placeholder"
+						className="card-image"
+					></img>
+					<div className="card-description">
+						<p className="cd-title">Description</p>
+						<p className="cd-description">
+							This will be the product description
+						</p>
+						<p className="cd-price">Price: $100.00</p>
+					</div>
+				</div>
 			</div>
-			<div>
-				Product 3 <Link to="/products/butter">Butter</Link>
-			</div>
-			<div>
-				Product 4 <Link to="/products/bread">Bread</Link>
-			</div>
-			<div>
-				Product 4 <Link to="/products/toast">Toast</Link>
+			<div className="card-row">
+				<div className="product-card">
+					<div className="card-title">
+						<Link to="/products/toast">LTO: Toast</Link>
+					</div>
+					<img
+						src={placeholder}
+						alt="placeholder"
+						className="card-image"
+					></img>
+					<div className="card-description">
+						<p className="cd-title">Description</p>
+						<p className="cd-description">
+							This will be the product description
+						</p>
+						<p className="cd-price">Price: $100.00</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
