21380a68c43493c0ea1e2092aef0d4dcafda7a81
rednirug <sgurinderpreet@gmail.com>
2020-11-15 22:06:32 -0800

    login/signup layout

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -4,10 +4,39 @@ import "./index.css";
 function Login() {
 	return (
 		<div className="login-container">
-			<p className="login-p">
-				Welcome to the Log In and Sign Up page. This page is still being
-				worked on. Thank you for your patience.
-			</p>
+			<div className="login-p">
+				<p>Login</p>
+				<form className="login-form">
+					<label for="email">Email:</label>
+					<br />
+					<input type="text" id="email" name="email"></input>
+					<br />
+					<label for="pword">Password:</label>
+					<br />
+					<input type="password" id="pword" name="pword"></input>
+					<input type="submit" value="Submit"></input>
+				</form>
+			</div>
+			<div className="login-or">
+				<p>OR</p>
+			</div>
+			<div className="signup-p">
+				<p>Sign Up</p>
+				<form className="signup-form">
+					<label for="username">Username:</label>
+					<br />
+					<input type="text" id="username" name="username"></input>
+					<br />
+					<label for="email">Email:</label>
+					<br />
+					<input type="text" id="email" name="email"></input>
+					<br />
+					<label for="pword">Password:</label>
+					<br />
+					<input type="password" id="pword" name="pword"></input>
+					<input type="submit" value="Submit"></input>
+				</form>
+			</div>
 		</div>
 	);
 }
