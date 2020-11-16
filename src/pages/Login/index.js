8d6f1e1b562dc64a2cb43fa3788ee699a139bb2d
rednirug <sgurinderpreet@gmail.com>
2020-11-15 22:19:12 -0800

    login/signup form fixes

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -5,36 +5,81 @@ function Login() {
 	return (
 		<div className="login-container">
 			<div className="login-p">
-				<p>Login</p>
+				<p className="login-title">Login</p>
 				<form className="login-form">
-					<label for="email">Email:</label>
+					<label for="email" className="label-name">
+						Email:
+					</label>
 					<br />
-					<input type="text" id="email" name="email"></input>
+					<input
+						type="text"
+						id="email"
+						className="ls-input"
+						name="email"
+					></input>
 					<br />
-					<label for="pword">Password:</label>
+					<label for="pword" className="label-name">
+						Password:
+					</label>
 					<br />
-					<input type="password" id="pword" name="pword"></input>
-					<input type="submit" value="Submit"></input>
+					<input
+						type="password"
+						id="pword"
+						className="ls-input"
+						name="pword"
+					></input>
+					<br />
+					<input
+						type="submit"
+						className="ls-input ls-btn"
+						value="Submit"
+					></input>
 				</form>
 			</div>
 			<div className="login-or">
 				<p>OR</p>
 			</div>
 			<div className="signup-p">
-				<p>Sign Up</p>
+				<p className="login-title">Sign Up</p>
 				<form className="signup-form">
-					<label for="username">Username:</label>
+					<label for="username" className="label-name">
+						Username:
+					</label>
 					<br />
-					<input type="text" id="username" name="username"></input>
+					<input
+						type="text"
+						id="username"
+						className="ls-input"
+						name="username"
+					></input>
 					<br />
-					<label for="email">Email:</label>
+					<label for="email" className="label-name">
+						Email:
+					</label>
 					<br />
-					<input type="text" id="email" name="email"></input>
+					<input
+						type="text"
+						id="email"
+						className="ls-input"
+						name="email"
+					></input>
 					<br />
-					<label for="pword">Password:</label>
+					<label for="pword" className="label-name">
+						Password:
+					</label>
 					<br />
-					<input type="password" id="pword" name="pword"></input>
-					<input type="submit" value="Submit"></input>
+					<input
+						type="password"
+						id="pword"
+						className="ls-input"
+						name="pword"
+					></input>
+					<br />
+					<input
+						type="submit"
+						className="ls-input ls-btn"
+						value="Submit"
+					></input>
 				</form>
 			</div>
 		</div>
